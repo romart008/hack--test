d923fab93ed72c2e608bdacdad01d46e459ec015
romart008 <martunykr9@gmail.com>
2026-04-05 23:31:06 +0300

    backend: Додав підтримку SQLite

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(f'database/main.db')
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+        
+def query_db(query, args=(), one=False):
+    db = get_db()
+    cur = db.execute(query, args)
+    rv = cur.fetchall()
+    db.commit()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 amount_per_delivery = 20
 days_per_delivery = 2
